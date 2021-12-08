84ba95cc3ed33495da8b171e9c9a01e5b1d0d186
Ceren <battal.ceren@gmail.com>
2021-12-08 14:04:44 +0100

    tidying up

--- src/create_pRFlogfiles_01.m
+++ src/create_pRFlogfiles_01.m
@@ -1,23 +1,23 @@
 function create_pRFlogfiles_01(action)
-%cb edited on 16.10.2018 
+%cb edited on 16.10.2018
 % this function created logfiles for pRF mrVista analysis.
 % for now it needs to be in the same folder as the .mat files.
 % for now, it only makes TR -- condition onset files
 % later on actualy time (in seconds) ca nbe added)
 
 % action == 1 makes stimTR with the logfile as input
-% action == 2 makes stimuli space across time 
+% action == 2 makes stimuli space across time
 
 % cb 27.02.2019
-% making logfiles - fMRI and Stimuli folders. 
-% transfering logfiles from spatiotopy_cer to fMRI 
+% making logfiles - fMRI and Stimuli folders.
+% transfering logfiles from spatiotopy_cer to fMRI
 
 % cb 07.12.2021
 % general edit + replacing sphere with a grid of 5x5x160 grid
 
 %% set parameters
 subject = 'AlSapilot';
-group = 'SC'; 
+group = 'SC';
 
 data = '/Volumes/extreme/Cerens_files/fMRI/Processed/Spatio_pRF/';
 mainpath = fullfile(data,group,subject);
@@ -25,13 +25,14 @@ mainpath = fullfile(data,group,subject);
 gap = 12.5; % in seconds
 TR = 2.5; % in seconds
 runNb = 7;
- 
+
 % stimuli type = 1, it's circular
 % stimuli type = 2, it's one point in the grid
 stimuliType = 2;
 
 %% let's beging
 switch action
+    % make stimOrder from original logfiles
     case 1
         for iRun = 1:runNb
             
@@ -59,23 +60,20 @@ switch action
             
         end
         
+        % inserting 1s into stimuli grid
     case 2
         
-        for iRun = 1:runNb
-
-            % load the mat file
-            matFile = [subject, '_logfile', num2str(iRun)];
-            load(fullfile(mainpath,'logfiles', matFile), 'stimOrder');
+        if stimuliType == 1
             
-            % assign stimuli into number of frames
-%             frameNb = length(stimOrder); 
-            frameNb = length(unique(stimOrder)); 
-
-            % change 0 into 9 labelling for silences
-            stimOrder(stimOrder == 0) = 9;
-            
-            %% inserting 1s into stimuli grid
-            if stimuliType == 1
+            for iRun = 1:runNb
+                
+                % load the example mat file
+                matFile = [subject, '_logfile', num2str(iRun)];
+                load(fullfile(mainpath,'logfiles', matFile), 'stimOrder');
+                
+                
+                % assign stimuli into number of frames
+                frameNb = length(stimOrder);
                 
                 %radius of your stimuli in space
                 radius = 10;
@@ -86,9 +84,9 @@ switch action
                 % create empty grid
                 myGrid = linspace(-70,70,gridDimension);
                 [x,y] = meshgrid(myGrid, -myGrid);
-
+                
                 stimImg = zeros(length(myGrid)*length(myGrid),frameNb);
-            
+                
                 center_90deg = 60;
                 center_45deg = center_90deg/sqrt(2);
                 
@@ -135,81 +133,94 @@ switch action
                     end
                 end
                 
-            % restucture our stim space
-            for iFrame = 1:frameNb
-                images(:,:,iFrame) = reshape(stimImg(:,iFrame),gridDimension,gridDimension);
-            end
-            
-            % visualisation
-            for iFrame = 1:frameNb
-                figure(100)
-                imagesc(images(:,:,iFrame) );
-                colormap gray
-                axis square
-                drawnow
-                pause(0.1)
-            end
-            
-            % save           
-            newMatFile = ['images_pRF_run',num2str(iRun), '_r', num2str(radius)];
-            save(fullfile(mainpath, 'Stimuli',newMatFile),'images');
-
-                
-            
-            elseif stimuliType == 2
-
-                %grid dimension
-                gridDimension = 5;
-                
-                %create zero grid
-                myGrid = linspace(-1,1,gridDimension);
-
-                stimImg = zeros(length(myGrid),length(myGrid),frameNb);
-                
+                % restucture our stim space
                 for iFrame = 1:frameNb
-                    if iFrame == 1 % UP 90deg
-                        stimImg(1,3,iFrame)=1;
-                    end
-                    if iFrame == 2 % RU 45deg
-                        stimImg(2,4,iFrame)=1;
-                    end
-                    if iFrame == 3 % Right 0deg
-                        stimImg(3,5,iFrame)=1;
-                    end
-                    if iFrame == 4 % RD 315deg
-                        stimImg(4,4,iFrame)=1;
-                    end
-                    if iFrame == 5 % DOWN 270deg
-                        stimImg(5,3,iFrame)=1;
-                    end
-                    if iFrame == 6 % LD 225deg
-                        stimImg(4,2,iFrame)=1;
-                    end
-                    if iFrame == 7 % Left 180deg                        
-                        stimImg(3,1,iFrame)=1;
-                    end
-                    if iFrame == 8 % LU 135deg
-                        stimImg(2,2,iFrame)=1;
-                        
-                    end
+                    images(:,:,iFrame) = reshape(stimImg(:,iFrame),gridDimension,gridDimension);
+                end
+                
+                % visualisation
+                for iFrame = 1:frameNb
+                    figure(100)
+                    imagesc(images(:,:,iFrame) );
+                    colormap gray
+                    axis square
+                    drawnow
+                    pause(0.1)
+                end
+                
+                % save
+                newMatFile = ['images_pRF_run',num2str(iRun), '_r', num2str(radius)];
+                save(fullfile(mainpath, 'Stimuli',newMatFile),'images');
+                
+            end
+            
+        elseif stimuliType == 2
+            
+            % load the example mat file
+            iRun = 1;
+            matFile = [subject, '_logfile', num2str(iRun)];
+            load(fullfile(mainpath,'logfiles', matFile), 'stimOrder');
+            
+            % find the unique positions
+            frameNb = length(unique(stimOrder));
+            
+            % change 0 into 9 labelling for silences
+            stimOrder(stimOrder == 0) = 9;
+            
+            %grid dimension
+            gridDimension = 5;
+            
+            %create zero grid
+            myGrid = linspace(-1,1,gridDimension);
+            
+            stimImg = zeros(length(myGrid),length(myGrid),frameNb);
+            
+            for iFrame = 1:frameNb
+                if iFrame == 1 % UP 90deg
+                    stimImg(1,3,iFrame)=1;
+                end
+                if iFrame == 2 % RU 45deg
+                    stimImg(2,4,iFrame)=1;
+                end
+                if iFrame == 3 % Right 0deg
+                    stimImg(3,5,iFrame)=1;
+                end
+                if iFrame == 4 % RD 315deg
+                    stimImg(4,4,iFrame)=1;
+                end
+                if iFrame == 5 % DOWN 270deg
+                    stimImg(5,3,iFrame)=1;
+                end
+                if iFrame == 6 % LD 225deg
+                    stimImg(4,2,iFrame)=1;
+                end
+                if iFrame == 7 % Left 180deg
+                    stimImg(3,1,iFrame)=1;
+                end
+                if iFrame == 8 % LU 135deg
+                    stimImg(2,2,iFrame)=1;
+                    
                 end
             end
             
-%             
-%             % visualisation
-%             for iFrames = 1:frameNb
-%                 figure(100)
-%                 imagesc(stimImg(:,:,iFrames));
-%                 colormap gray
-%                 axis square
-%                 drawnow
-%                 pause(0.1)
-%             end
-
+            %
+            %             % visualisation
+            %             for iFrames = 1:frameNb
+            %                 figure(100)
+            %                 imagesc(stimImg(:,:,iFrames));
+            %                 colormap gray
+            %                 axis square
+            %                 drawnow
+            %                 pause(0.1)
+            %             end
+            
             % save as pRF analysis would like to have
             images = stimImg;
-            newMatFile = ['images_pRF_run',num2str(iRun)];
+            newMatFile = 'images_pRF';
             save(fullfile(mainpath, 'Stimuli',newMatFile),'images');
             
         end
+        
+        
+        
 end
