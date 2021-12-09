fac6600d96a80c73775f57f68c00007b50c08696
Ceren <battal.ceren@gmail.com>
2021-12-09 13:58:17 +0100

    adding example call to run

--- src/run_pRFmodeling_03.m
+++ src/run_pRFmodeling_03.m
@@ -1,3 +1,16 @@
+
+clear all;
+close all;
+
+%add path mrvista
+mrvistaPath = '/Users/battal/Documents/MATLAB/vistasoft';
+addpath(genpath(mrvistaPath));
+
+% go to subject's folder
+subjectPath = '/Volumes/extreme/Cerens_files/fMRI/Processed/Spatio_pRF/SC/AlSapilot';
+cd(subjectPath);
+
+
 % rmMain([1 3],ROIname,5,'model',{'one gaussian'},'hrf',{'t',[5.4000 5.2000 10.8000 7.3500 0.35000]},'matfilename','ONEGAUSSIAN_wholebrain');
 
 %rmMain([1 3],[],5,'model',{'one gaussian'},'hrf',{'t',[5.4000 5.2000 10.8000 7.3500 0.35000]},'matfilename','ONEGAUSSIAN_wholebrain');
@@ -10,3 +23,7 @@ rmMain([1 6],'left_Auditory',5,'model',{'one gaussian'},'hrf',{'t',[5.4000 5.200
 
 %AlSa
 rmMain([1 7],'left_Auditory',5,'model',{'one gaussian'},'hrf',{'t',[5.4000 5.2000 10.8000 7.3500 0.35000]},'matfilename','ONEGAUSSIAN_left_aud2');
+
+
+% 08.12.2021
+rmMain([1 3],[],5,'model',{'one gaussian'},'hrf',{'t',[5.4000 5.2000 10.8000 7.3500 0.35000]},'matfilename','ONEGAUSSIAN_wholebrain', 'stimx', [-1 -1/sqrt(2) 0 1/sqrt(2) 1], 'stimy', [-1 -1/sqrt(2) 0 1/sqrt(2) 1]);
