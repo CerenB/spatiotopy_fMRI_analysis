5dce9c6cfd826212d204deba3c2931690a581a37
CerenB <battal.ceren@gmail.com>
2021-12-13 11:37:40 +0100

    clean run script

--- src/run_pRFmodeling_03.m
+++ src/run_pRFmodeling_03.m
@@ -2,28 +2,25 @@
 clear all;
 close all;
 
+pc = 2;
+mrvistaPath = '/data2/spatiotopy/code/vistasoft';
+subjectPath = '/data2/spatiotopy/raw/SC/AlSapilot';
+
+if pc == 1
+    mrvistaPath = '/Users/battal/Documents/MATLAB/vistasoft';
+    subjectPath = '/Volumes/extreme/Cerens_files/fMRI/Processed/Spatio_pRF/SC/AlSapilot';
+
+end
 %add path mrvista
-mrvistaPath = '/Users/battal/Documents/MATLAB/vistasoft';
 addpath(genpath(mrvistaPath));
 
 % go to subject's folder
-subjectPath = '/Volumes/extreme/Cerens_files/fMRI/Processed/Spatio_pRF/SC/AlSapilot';
 cd(subjectPath);
 
 
-% rmMain([1 3],ROIname,5,'model',{'one gaussian'},'hrf',{'t',[5.4000 5.2000 10.8000 7.3500 0.35000]},'matfilename','ONEGAUSSIAN_wholebrain');
-
-%rmMain([1 3],[],5,'model',{'one gaussian'},'hrf',{'t',[5.4000 5.2000 10.8000 7.3500 0.35000]},'matfilename','ONEGAUSSIAN_wholebrain');
-rmMain([1 6],[],5,'model',{'one gaussian'},'hrf',{'t',[5.4000 5.2000 10.8000 7.3500 0.35000]},'matfilename','ONEGAUSSIAN_wholebrain');
-
-rmMain([1 6],'Right_Aud',5,'model',{'one gaussian'},'hrf',{'t',[5.4000 5.2000 10.8000 7.3500 0.35000]},'matfilename','ONEGAUSSIAN_right_aud');
-
-%cd /home/battal/Documents/AnBa
-rmMain([1 6],'left_Auditory',5,'model',{'one gaussian'},'hrf',{'t',[5.4000 5.2000 10.8000 7.3500 0.35000]},'matfilename','ONEGAUSSIAN_left_aud2');
-
-%AlSa
-rmMain([1 7],'left_Auditory',5,'model',{'one gaussian'},'hrf',{'t',[5.4000 5.2000 10.8000 7.3500 0.35000]},'matfilename','ONEGAUSSIAN_left_aud2');
-
-
 % 08.12.2021
-rmMain([1 3],[],5,'model',{'one gaussian'},'hrf',{'t',[5.4000 5.2000 10.8000 7.3500 0.35000]},'matfilename','ONEGAUSSIAN_wholebrain', 'stimx', [-1 -1/sqrt(2) 0 1/sqrt(2) 1], 'stimy', [-1 -1/sqrt(2) 0 1/sqrt(2) 1]);
+rmMain([1 3],[],1,'model',{'one gaussian'},'hrf',...
+    {'t',[5.4000 5.2000 10.8000 7.3500 0.35000]},...
+    'matfilename','ONEGAUSSIAN_wholebrain', ...
+    'stimx', [-1 -1/sqrt(2) 0 1/sqrt(2) 1], 'stimy', [-1 -1/sqrt(2) 0 1/sqrt(2) 1], ...
+    'minrf', 0.05, 'maxrf', 2, 'numbersigmas', 79, 'relativeGridStep', 1, 'coarsetofine',0, 'coarsesample',0);
