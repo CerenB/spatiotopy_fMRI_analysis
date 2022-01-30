f2a29a755710d6476e18ae4951567086a56df7d1
Ceren <battal.ceren@gmail.com>
2022-01-30 20:24:13 +0100

    clean, comment, add even/odd option

--- src/changePath.m
+++ src/changePath.m
@@ -1,7 +1,11 @@
 
-% changing dataTYPES input 
+% script is for changing dataTYPES input/paths
 
-% load a session from ubuntu PC
+% load a session from ubuntu PC. which we already RUN pRF modeling with the
+% paths set according to ubuntu PC
+% we will use this paths and adjust with our new subject name
+
+% load old subject from ubuntu
 oldSubject = 'DaBe';
 load(['/Volumes/extreme/Cerens_files/fMRI/Processed/Spatio_pRF/SC/', ...
     oldSubject,'/studentUbuntu/mrSESSION.mat'])
@@ -12,11 +16,6 @@ individualRuns = dataTYPES(3).retinotopyModelParams;
 % save its datatypes - averaged runs
 averagedRuns = dataTYPES(5).retinotopyModelParams;
 
-% dataTYPES(7).retinotopyModelParams.imFile
-%  
-% dataTYPES(7).retinotopyModelParams.jitterFile
-% 
-% dataTYPES(7).retinotopyModelParams.paramsFile
 
 
 %load new subject' session:
@@ -30,17 +29,17 @@ vANATOMYPATH = ['/data2/spatiotopy/raw/SC/',subject,...
                 '/sSPATIO_pRF_',subject,'.nii'];
 
 %% assigned averaged runs
-AverageInd = length(dataTYPES);
-dataTYPES(AverageInd).retinotopyModelParams = averagedRuns;
+dataTypeInd = 6;
+dataTYPES(dataTypeInd).retinotopyModelParams = averagedRuns;
 
 % now changed the paths:
-dataTYPES(AverageInd).retinotopyModelParams.paramsFile = ...
+dataTYPES(dataTypeInd).retinotopyModelParams.paramsFile = ...
     ['/data2/spatiotopy/raw/SC/',subject,'/Stimuli/params_pRF_averageAll.mat'];
 
-dataTYPES(AverageInd).retinotopyModelParams.jitterFile = ...
+dataTYPES(dataTypeInd).retinotopyModelParams.jitterFile = ...
     ['/data2/spatiotopy/raw/SC/',subject,'/Stimuli/none'];
 
-dataTYPES(AverageInd).retinotopyModelParams.imFile = ...
+dataTYPES(dataTypeInd).retinotopyModelParams.imFile = ...
     ['/data2/spatiotopy/raw/SC/',subject,'/Stimuli/images_pRF_average.mat'];
 
 
@@ -72,6 +71,37 @@ end
 
 save(path2Session, 'mrSESSION', 'dataTYPES', 'vANATOMYPATH');
 
+%% assigned even and odd runs
+dataTypeInd = 7; % 7 is Odd runs, and 8 is even in AnTo
+dataTYPES(dataTypeInd).retinotopyModelParams = averagedRuns;
+
+% now changed the paths:
+dataTYPES(dataTypeInd).retinotopyModelParams.paramsFile = ...
+    ['/data2/spatiotopy/raw/SC/',subject,'/Stimuli/params_pRF_averageOdd.mat'];
+
+dataTYPES(dataTypeInd).retinotopyModelParams.jitterFile = ...
+    ['/data2/spatiotopy/raw/SC/',subject,'/Stimuli/none'];
+
+dataTYPES(dataTypeInd).retinotopyModelParams.imFile = ...
+    ['/data2/spatiotopy/raw/SC/',subject,'/Stimuli/images_pRF_averageOdd.mat'];
+
+% save the mrSESSION.mat
+save(path2Session, 'mrSESSION', 'dataTYPES', 'vANATOMYPATH');
+
+% now even runs
+dataTypeInd = 8;
+dataTYPES(dataTypeInd).retinotopyModelParams = averagedRuns;
+
+% now changed the paths:
+dataTYPES(dataTypeInd).retinotopyModelParams.paramsFile = ...
+    ['/data2/spatiotopy/raw/SC/',subject,'/Stimuli/params_pRF_averageEven.mat'];
+
+dataTYPES(dataTypeInd).retinotopyModelParams.jitterFile = ...
+    ['/data2/spatiotopy/raw/SC/',subject,'/Stimuli/none'];
+
+dataTYPES(dataTypeInd).retinotopyModelParams.imFile = ...
+    ['/data2/spatiotopy/raw/SC/',subject,'/Stimuli/images_pRF_averageEven.mat'];
 
 
-
+% save the mrSESSION.mat
+save(path2Session, 'mrSESSION', 'dataTYPES', 'vANATOMYPATH');
